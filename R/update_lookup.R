02c3c68d375f351b40a6ac6dbaf2e55a3e6aa047
Rasmus Puggaard-Rode <r.puggaard@phonetik.uni-muenchen.de>
2023-01-03 16:01:11 +0100

    First commit

---
+++ R/update_lookup.R
@@ -0,0 +1,20 @@
+update_lookup <- function() {
+
+  tmp <- sampa_lookup
+
+  tmp[,'length'] <- 'short'
+  tmp[,'modifications'] <- NA
+  tmp[,'syllabic'] = dplyr::case_when(
+    tmp[,'manner'] == 'vowel' ~ 'yes',
+    TRUE ~ 'no')
+  tmp[,'release'] <- dplyr::case_when(
+    tmp[,'major_manner'] == 'obstruent' ~ 'released',
+    TRUE ~ 'NA')
+  tmp[,'nasalization'] <- dplyr::case_when(
+    tmp[,'manner'] == 'nasal' ~ 'yes',
+    TRUE ~ 'no')
+  tmp[,'tone'] <- NA
+
+  return(tmp)
+
+}
